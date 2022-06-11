66df8372fad87f74c2353d53003a745201dc8c8a
Ouail Zahir <ozahir@e3r1p13.1337.ma>
2022-06-11 15:58:14 +0100

    gnl

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozahir <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/05 09:55:33 by ozahir            #+#    #+#             */
+/*   Updated: 2021/12/08 02:35:00 by ozahir           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include	<stdlib.h>
+# include	<unistd.h>
+# include	<fcntl.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+int		ft_strlen(char const *s);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char *s1, char const *s2);
+char	*newline(char *left);
+int		nl_sig(char *str);
+char	*get_next_line(int fd);
+char	*rest(char	*rest);
+
+#endif
